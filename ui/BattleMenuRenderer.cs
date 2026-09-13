ffb2a8be8027a691c550d3f5676ff54a054003ad
robertmccarn <54292691+robertmccarn@users.noreply.github.com>
2026-09-12 21:45:00 -0500

    Remove obsolete atlas dependency from battle menu

--- ui/BattleMenuRenderer.cs
+++ ui/BattleMenuRenderer.cs
@@ -2,7 +2,6 @@ using System.Drawing;
 
 public class BattleMenuRenderer : IDisposable
 {
-    private readonly AtlasSlicer atlas = new();
     private readonly Font font = new(FontFamily.GenericMonospace, 16);
 
     public void DrawCommands(Graphics graphics, BattleCommand selectedCommand)
@@ -24,26 +23,12 @@ public class BattleMenuRenderer : IDisposable
             BattleCommand command = commands[index];
             int y = 402 + index * 29;
             bool isSelected = command == selectedCommand;
-            AtlasCommandIcon icon = command switch
-            {
-                BattleCommand.Attack => AtlasCommandIcon.Attack,
-                BattleCommand.Skill => AtlasCommandIcon.Tech,
-                BattleCommand.Item => AtlasCommandIcon.Item,
-                BattleCommand.Interact => AtlasCommandIcon.Interact,
-                BattleCommand.Defend => AtlasCommandIcon.Defend,
-                BattleCommand.Run => AtlasCommandIcon.Retreat,
-                _ => AtlasCommandIcon.Interact
-            };
 
             if (isSelected)
-            {
                 graphics.FillRectangle(Brushes.DarkSlateBlue, 52, y - 2, 380, 27);
-            }
 
-            AtlasRegion region = atlas.GetCommandIconRegion(icon);
-            atlas.Draw(graphics, region, new Rectangle(60, y, 24, 24));
             graphics.DrawString($"{(isSelected ? "▶" : " ")} {command}", font,
-                isSelected ? Brushes.White : Brushes.LightGray, 95, y + 3);
+                isSelected ? Brushes.White : Brushes.LightGray, 65, y + 3);
         }
     }
 
@@ -52,15 +37,14 @@ public class BattleMenuRenderer : IDisposable
         int offsetX = 0;
         foreach (StatusEffect effect in character.StatusEffects)
         {
-            AtlasRegion region = atlas.GetStatusIconRegion((AtlasStatusIcon)effect);
-            atlas.Draw(graphics, region, new Rectangle(x + offsetX, y, 16, 16));
+            graphics.FillRectangle(Brushes.DarkRed, x + offsetX, y, 16, 16);
+            graphics.DrawRectangle(Pens.White, x + offsetX, y, 16, 16);
             offsetX += 18;
         }
     }
 
     public void Dispose()
     {
-        atlas.Dispose();
         font.Dispose();
     }
 }
