2a1b3753ca73718a4d9a21003c9ab32e23a4562b
robertmccarn <54292691+robertmccarn@users.noreply.github.com>
2026-09-20 17:30:13 -0500

    Clarify StaticUnit node metadata ownership

--- game/StaticUnit.cs
+++ game/StaticUnit.cs
@@ -1,9 +1,12 @@
 using Systemic.Engine.State;
 
+// StaticUnit is non-interactive world metadata for a DungeonNode.
+// InteractiveProp owns player interaction; StaticUnit does not duplicate it.
 public class StaticUnit : GameObject
 {
     public bool IsBlocking { get; }
     public DungeonNodeType NodeType { get; }
+    public string NodeId { get; }
     public bool IsActivated { get; private set; }
 
     public StaticUnit(
@@ -11,11 +14,13 @@ public class StaticUnit : GameObject
         int x,
         int y,
         bool isBlocking,
-        DungeonNodeType nodeType)
+        DungeonNodeType nodeType,
+        string nodeId = "")
         : base(name, x, y)
     {
         IsBlocking = isBlocking;
         NodeType = nodeType;
+        NodeId = nodeId;
     }
 
     public void Activate() => IsActivated = true;
